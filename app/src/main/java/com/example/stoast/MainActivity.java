45b8fcff68cb4967035843f6eaa31d975393cdf0
AshrafulAlamS <mdashrafulalam44@gmail.com>
2020-10-06 02:47:59 +0600

    initial custom toast added

--- app/src/main/java/com/example/stoast/MainActivity.java
+++ app/src/main/java/com/example/stoast/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.stoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn=findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater inflater=getLayoutInflater();
+
+               View stView = inflater.inflate(R.layout.stoast_layout, (ViewGroup) findViewById(R.id.stLayout));
+
+                Toast toast = new Toast(MainActivity.this);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.setView(stView);
+                toast.show();
+            }
+        });
     }
 }
